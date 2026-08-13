c37d63489937a5817742f6f222a54ab79fa502da
Jalagon <A1ag0nJus1in@gmail.com>
2026-08-13 13:21:56 -0700

    UndergroundBg GE

--- Assets/DeepSeaGame/Scripts/Background/UndergroundBackground.cs
+++ Assets/DeepSeaGame/Scripts/Background/UndergroundBackground.cs
@@ -9,26 +9,16 @@ namespace DeepSeaGame
         [SerializeField] private GameObject _camera;
         [SerializeField] private float _parallaxEffect;
 
-        private float _length, _startPos, _yCenterLevel;
+        private float _startPos, _yCenterLevel;
         private bool _initialized;
 
         private void FixedUpdate()
         {
             if (!_initialized) return;
 
-            float temp = _camera.transform.position.x * (1 - _parallaxEffect);
             float distance = _camera.transform.position.x * _parallaxEffect;
 
             transform.position = new Vector3(_startPos + distance, _yCenterLevel, transform.position.z);
-
-            if (temp > _startPos + _length)
-            {
-                _startPos += _length;
-            }
-            else if (temp < _startPos - _length)
-            {
-                _startPos -= _length;
-            }
         }
 
         public void Initialize(WorldGenerationData worldGenerationData)
@@ -40,8 +30,7 @@ namespace DeepSeaGame
         {
             yield return new WaitForSeconds(1f);
 
-            _startPos = transform.position.x - _camera.transform.position.x * _parallaxEffect;
-            _length = GetComponent<SpriteRenderer>().bounds.size.x;
+            _startPos = transform.position.x + (_camera.transform.position.x * _parallaxEffect);
             
             float undergroundHeight = data.UndergroundMaxYLevel - data.UndergroundMinYLevel;
             _yCenterLevel = (undergroundHeight * 0.5f) + data.UndergroundMinYLevel;
